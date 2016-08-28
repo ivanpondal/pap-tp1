c35d91746f28639278d2fd371446f3f81fcb6fc1
Iván Pondal <ivan.pondal@gmail.com>
2016-08-28 12:15:45 -0300

    Refactor exercise 4 solution

--- src/ej4/solver.cpp
+++ src/ej4/solver.cpp
@@ -8,8 +8,32 @@ using namespace std;
 **  Problem solver
 */
 
+int max(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+int min(int a, int b) {
+	return (a < b) ? a : b;
+}
+
+void calculate_matrix_products(Matriz *arr_m, Matriz *arr_prod_m, int i, int j, bool right_half) {
+	if (right_half) {
+		arr_prod_m[i] = arr_m[i];
+		for (int k = i + 1; k < j; k++) {
+			arr_prod_m[k] = arr_prod_m[k - 1] * arr_m[k];
+		}
+	}
+	else {
+		arr_prod_m[j - 1] = arr_m[j - 1];
+		for (int k = j - 2; k >= i; k--) {
+			arr_prod_m[k] = arr_m[k] * arr_prod_m[k + 1];
+		}
+	}
+}
+
 bool subarray_exists(Matriz *arr_m, Matriz &m, Matriz *arr_prod_m,
-				     int n, int l, int i, int j, bool right_half, int *sol_i, int *sol_j) {
+		int n, int l, int i, int j, bool right_half, int *sol_i, int *sol_j) {
+
 	if (n == 1) {
 		if (l == 1 && arr_m[i] == m) {
 			*sol_i = i;
@@ -23,58 +47,32 @@ bool subarray_exists(Matriz *arr_m, Matriz &m, Matriz *arr_prod_m,
 	else {
 		if (l <= n) {
 			int middle = n / 2;
-			bool sol_right_half = subarray_exists(arr_m, m, arr_prod_m, n - middle, l, i + middle, j, true, sol_i, sol_j);
-			bool sol_left_half = subarray_exists(arr_m, m, arr_prod_m, middle, l, i, i + middle, false, sol_i, sol_j);
+
+			bool sol_right_half = subarray_exists(
+					arr_m, m, arr_prod_m, n - middle, l, i + middle, j, true, sol_i, sol_j);
+
+			bool sol_left_half = subarray_exists(
+					arr_m, m, arr_prod_m, middle, l, i, i + middle, false, sol_i, sol_j);
 
 			if (sol_right_half || sol_left_half) {
 				return true;
 			}
 			else {
-				if (l <= middle) {
-					for (int k = i + middle; k < i + middle + l - 1; k++) {
-						if (arr_prod_m[k - l + 1] * arr_prod_m[k] == m) {
-							*sol_i = k - l + 1;
-							*sol_j = k + 1;
-							return true;
-						}
+				int start = max(i + middle, i + l - 1);
+				int end = min(i + middle + l - 1, j);
+
+				for (int k = start; k < end; k++) {
+					if (arr_prod_m[k - l + 1] * arr_prod_m[k] == m) {
+						*sol_i = k - l + 1;
+						*sol_j = k + 1;
+						return true;
 					}
 				}
-				else {
-					for (int k = i + l - 1; k < j; k++) {
-						if (arr_prod_m[k - l + 1] * arr_prod_m[k] == m) {
-							*sol_i = k - l + 1;
-							*sol_j = k + 1;
-							return true;
-						}
-					}
-				}
-				if (right_half) {
-					arr_prod_m[i] = arr_m[i];
-					for (int k = i + 1; k < j; k++) {
-						arr_prod_m[k] = arr_prod_m[k - 1] * arr_m[k];
-					}
-				}
-				else {
-					arr_prod_m[j - 1] = arr_m[j - 1];
-					for (int k = j - 2; k >= i; k--) {
-						arr_prod_m[k] = arr_m[k] * arr_prod_m[k + 1];
-					}
-				}
+				calculate_matrix_products(arr_m, arr_prod_m, i, j, right_half);
 			}
 		}
 		else {
-			if (right_half) {
-				arr_prod_m[i] = arr_m[i];
-				for (int k = i + 1; k < j; k++) {
-					arr_prod_m[k] = arr_prod_m[k - 1] * arr_m[k];
-				}
-			}
-			else {
-				arr_prod_m[j - 1] = arr_m[j - 1];
-				for (int k = j - 2; k >= i; k--) {
-					arr_prod_m[k] = arr_m[k] * arr_prod_m[k + 1];
-				}
-			}
+			calculate_matrix_products(arr_m, arr_prod_m, i, j, right_half);
 		}
 	}
 	return false;
@@ -98,14 +96,5 @@ void run_solver() {
 
 	bool solution = subarray_exists(arr_m, m, arr_prod_m, n, l, 0, n, true, &sol_i, &sol_j);
 
-	if (solution) {
-		Matriz m_check = arr_m[sol_i];
-		cout << sol_i << " " << sol_j << endl;
-		for (int i = sol_i + 1; i < sol_j; i++) {
-			m_check *= arr_m[i];
-		}
-		cout << (m_check == m ? "OK" : "FAIL") << endl;
-	}
-
-	cout << (solution ? "yes" : "no") << endl;
+	cout << (solution ? "SI" : "NO") << endl;
 }
