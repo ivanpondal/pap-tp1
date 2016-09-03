81e461deb6d888bfe519facb91bfe58810cde4c1
Shai <shai.bianchi@mercadolibre.com>
2016-09-03 17:08:55 -0300

    [Ejercicio 3 - src] Add further test cases

--- src/ej3/test.cpp
+++ src/ej3/test.cpp
@@ -15,11 +15,12 @@ vector<T> arr2vec(int n, T arr[]) {
 void run_unit_tests() {
     // create test cases
     vector<test_case> cases =
-        arr2vec(4, (test_case[]) {
-            {.data = arr2vec(3, (int[]){2,-1,2}), .expected = 3}, // sumar todos
-            {.data = arr2vec(4, (int[]){-1,-1,-1,-1}), .expected = 0}, // no sumar ninguno
-            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,-4,-5,1,2,3,4}), .expected = 15}, // sumar primeros 5
-            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,6,-5,1,2,3,4}), .expected = 20}, // sumar todos
+        arr2vec(6, (test_case[]) {
+            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,6,-5,1,2,3,4}), .expected = 20}, // el óptimo es todo el arreglo
+            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,-4,-5,1,2,3,4}), .expected = 15}, // el óptimo es un prefijo
+            {.data = arr2vec(0, (int[]){}), .expected = 0}, // arreglo vacío, el óptimo es el vacío
+            {.data = arr2vec(4, (int[]){-1,-1,-1,-1}), .expected = 0}, // el óptimo es el vacío en arreglo no vacío
+            {.data = arr2vec(10, (int[]){1,2,-7,1,2,3,4,5,-16,2}), .expected = 15}, // subarreglo óptimo que no es prefijo ni sufijo
         });
 
     // run test cases
