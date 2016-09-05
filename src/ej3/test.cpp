6980e7f6df3dbe5ff5503641f0f4e2fec5683045
Shai <shai.bianchi@mercadolibre.com>
2016-09-04 22:24:06 -0300

    [Exercise 3 - src] Tiny refactor in tests

--- src/ej3/test.cpp
+++ src/ej3/test.cpp
@@ -13,7 +13,7 @@ vector<T> arr2vec(int n, T arr[]) {
 }
 
 // fills out a given vector with random positive numbers and returns their sum
-int randomPositiveVectorFillAndSum(vector<int>& v) {
+int randomNonNegativeVectorFillAndSum(vector<int>& v) {
     // calculate an upper bound for vector elements so they are uniformly distributed and their sum does not overflow int capacity
 	int upperBound = (int)((1<<31)-1) / v.size();
 	int sum = 0;
@@ -29,14 +29,14 @@ int randomPositiveVectorFillAndSum(vector<int>& v) {
 void run_unit_tests() {
     // create test cases
     vector<int> loadTestData(INPUT_SIZE_UPPER_BOUND);
-    int loadTestExpected = randomPositiveVectorFillAndSum(loadTestData);
+    int loadTestExpected = randomNonNegativeVectorFillAndSum(loadTestData);
 
     vector<test_case> cases =
         arr2vec(6, (test_case[]) {
-            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,6,-5,1,2,3,4}), .expected = 20}, // el óptimo es todo el arreglo
-            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,-4,-5,1,2,3,4}), .expected = 15}, // el óptimo es un prefijo
             {.data = arr2vec(0, (int[]){}), .expected = 0}, // arreglo vacío, el óptimo es el vacío
             {.data = arr2vec(4, (int[]){-1,-1,-1,-1}), .expected = 0}, // el óptimo es el vacío en arreglo no vacío
+            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,6,-5,1,2,3,4}), .expected = 20}, // el óptimo es todo el arreglo
+            {.data = arr2vec(14, (int[]){1,2,3,4,5,-1,-2,-3,-4,-5,1,2,3,4}), .expected = 15}, // el óptimo es un prefijo
             {.data = arr2vec(10, (int[]){1,2,-7,1,2,3,4,5,-16,2}), .expected = 15}, // subarreglo óptimo que no es prefijo ni sufijo
             {.data = loadTestData, .expected = loadTestExpected},
         });
